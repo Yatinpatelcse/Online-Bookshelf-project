33ef8dafacf88a967eedc0b97a288af22049c67a
Yatinpatelcse <yatinpatelcse@gmail.com>
2014-07-21 00:02:41 -0700

    first commit

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ActiveRecord::Base
+  validates :title,:description,:image_url,:price, presence: true
+  validates :title, uniqueness: true, length: {maximum: 50}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :discount_price,allow_blank: true, numericality: {greater_than_or_equal_to: 0.00}
+  validates :image_url, format: {
+  with: %r{\.(gif|jpg|png)\Z}i,
+  message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+end
